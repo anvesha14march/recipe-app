918855a1dfdb8d344c79c4753868d643acaf699a
anvesha14march <anvesha14march@gmail.com>
2022-05-14 19:44:53 +0000

    added backend firebase

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -7,21 +7,28 @@ import { Subject } from 'rxjs';
 @Injectable()
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
-  private recipes: Recipe[] = [
-    new Recipe(
-      'Creamy Dill Cucumber Toasties',
-      'It looks great on the platter, and it is super easy!',
-      'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fimages.media-allrecipes.com%2Fuserphotos%2F8507529.jpg&w=595&h=595&c=sc&poi=face&q=60',
-      [new Ingrident('cucumber', 1), new Ingrident('Cream cheese', 1)]
-    ),
-    new Recipe(
-      'Creamy Cheesy Spinach Dip',
-      " it's very cheesy and very delicious",
-      'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fimages.media-allrecipes.com%2Fuserphotos%2F8911615.jpg&w=595&h=595&c=sc&poi=face&q=60',
-      [new Ingrident('onion', 1), new Ingrident('Frozen Spanich', 2)]
-    ),
-  ];
+  // private recipes: Recipe[] = [
+  //   new Recipe(
+  //     'Creamy Dill Cucumber Toasties',
+  //     'It looks great on the platter, and it is super easy!',
+  //     'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fimages.media-allrecipes.com%2Fuserphotos%2F8507529.jpg&w=595&h=595&c=sc&poi=face&q=60',
+  //     [new Ingrident('cucumber', 1), new Ingrident('Cream cheese', 1)]
+  //   ),
+  //   new Recipe(
+  //     'Creamy Cheesy Spinach Dip',
+  //     " it's very cheesy and very delicious",
+  //     'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fimages.media-allrecipes.com%2Fuserphotos%2F8911615.jpg&w=595&h=595&c=sc&poi=face&q=60',
+  //     [new Ingrident('onion', 1), new Ingrident('Frozen Spanich', 2)]
+  //   ),
+  // ];
+  private recipes: Recipe[] = [];
   constructor(private slService: ShoppingListService) {}
+
+  setRecipes(recipes: Recipe[]) {
+    this.recipes = recipes;
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
   getRecipes() {
     return this.recipes.slice();
   }
